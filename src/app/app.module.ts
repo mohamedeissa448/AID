bb0a95bcd3a0c6b6b61097e7c3f8efb7c5814a94
mohamedeissa448 <mohamedeissa448@gmail.com>
2020-04-04 15:20:57 +0200

    add login component with authentication service with configuration file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule}from"@angular/forms"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import{ HttpClientModule}from"@angular/common/http"
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { settings } from '../config';
+import { Router } from '@angular/router';
+import { map } from "rxjs/operators";
+import { JwtHelperService } from "@auth0/angular-jwt";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient,private router:Router) { }
+  login(credentials){
+     /**token-payload={
+  "roles": ["admin"],
+   "id":"123",
+  "name": "admin",
+  "iat": 1516239022
+  } */
+    this.http.post(settings.serverUrl,credentials).pipe(map((response :any)=>{
+      response.token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJhZG1pbiJdLCJpZCI6IjEyMyIsIm5hbWUiOiJhZG1pbiIsImlhdCI6MTUxNjIzOTAyMn0.kAaEJz6ca2CjXmNOI1nmwmINF7bf4CPzHjYPWSjn4a0"
+    if (response && response.token) {
+   localStorage.setItem("token", response.token);
+   return true;
+ }
+ return false;
+}))
+    
+    
+  }
+  logOut() {
+    this.router.navigate(["authentication/page-login"]);
+    localStorage.removeItem("token");
+  }
+  isLogedIn() {
+    let jwtHelper = new JwtHelperService();
+    const token = localStorage.getItem("token");
+    if (!token) return false;
+    //console.log("token", token);
+    // console.log("isexpired", jwtHelper.isTokenExpired(token));
+
+    return !jwtHelper.isTokenExpired(token);
+  }
+  get currentUser() {
+    let token = localStorage.getItem("token");
+    if (!token) return null;
+    //console.log("decoded token ", new JwtHelperService().decodeToken(token));
+    return new JwtHelperService().decodeToken(token);
+  }
+}
